13dce96144fbcae3ab228fd0ea0056c687ac6ee6
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-04-03 19:26:39 +0000

    Deploying to docs from @ books-are-next/zidovske-anekdoty@29ddb1074e992d696cae927624824972273d8276 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-4b1d6cd';
+const CACHE = 'cache-29ddb10';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -40,7 +40,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./zidovske_anekdoty_001.html","./zidovske_anekdoty_002.html","./zidovske_anekdoty_003.html","./zidovske_anekdoty_004.html","./zidovske_anekdoty_005.html","./zidovske_anekdoty_006.html","./zidovske_anekdoty_007.html","./zidovske_anekdoty_008.html","./zidovske_anekdoty_009.html","./zidovske_anekdoty_010.html","./zidovske_anekdoty_011.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/obalka.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./zidovske_anekdoty_002.html","./zidovske_anekdoty_005.html","./zidovske_anekdoty_006.html","./zidovske_anekdoty_007.html","./zidovske_anekdoty_008.html","./zidovske_anekdoty_009.html","./zidovske_anekdoty_010.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/obalka.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
 }
 
 self.addEventListener('fetch', e => {
